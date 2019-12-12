5d17762e79d68e1b6152c3523b8778ad918d8038
Chaitanya98 <51097058+Chaitanya98@users.noreply.github.com>
2019-12-12 22:36:24 +0530

    Create Case 1.py

---
+++ 15) KNN Classification/Case 1.py
@@ -0,0 +1,13 @@
+from sklearn.datasets import load_iris
+
+iris_dataset = load_iris()
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+for val in y_pred:
+	print(iris_dataset['target_names'][val])
+print("Model Accuracy: {:.2f}".format(knn.score(X_test, y_test)))
